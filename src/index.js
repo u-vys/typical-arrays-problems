033ab71941d806d33914585635ebc835e0d64298
u-vys <vn.vysotski@gmail.com>
2021-03-11 22:24:27 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  let resultMin;
+  if ((array === undefined) || (array.length === 0)) resultMin = 0; 
+      else { resultMin = array [0];
+        for (let elem of array) {
+        if (elem < resultMin) resultMin = elem;
+      }
+      } 
+ return resultMin;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let resultMax;
+ 
+  if ((array === undefined) || (array.length === 0)) resultMax = 0; 
+      else { resultMax = array [0];
+        for (let elem of array) {
+        if (elem > resultMax) resultMax = elem;
+      }
+      } 
+
+
+  return resultMax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+ let result = 0; 
+ if ((array === undefined) || (array.length === 0) ) result = 0; 
+      else { for (let elem of array) result += elem;
+      result = result / array.length}
+         
+   return result;   
+      
 }
